a95ac720cc011c87e14e2a90694e44c952d87298
jesstie <hilalulfadli12@gmail.com>
2025-11-20 13:51:56 +0700

    Tambah overlay login admin dan penyesuaian

--- mainPage/script.js
+++ mainPage/script.js
@@ -2,6 +2,28 @@ function goto(page) {
     window.location.href = page;
 }
 
+function login() {
+    const usernameInput = document.getElementById("usn");
+    const passwordInput = document.getElementById("pwd");
+
+    const username = usernameInput.value;
+    const password = passwordInput.value;
+
+    if (!username || !password) {
+        alert("Username dan Password tidak boleh kosong!");
+        return;
+    }
+
+    if (username == "admin" && password == "admin123") {
+        goto("admin.html");
+    } else {
+        alert("Username atau Password salah!");
+    }
+
+    usernameInput.value = "";
+    passwordInput.value = "";
+}
+
 const popup = document.getElementById('loginPopup');
 const overlay = document.getElementById('loginOverlay');
 const openBtn = document.getElementById('adminLogo');
